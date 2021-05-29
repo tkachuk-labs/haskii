69c03df53a6b42f1ff7b9b76e9d72729b600d51b
tkachuk-labs <tkachuk.labs@gmail.com>
2021-05-29 13:37:04 +0000

    HaskiiFigletGen WIP

--- nix/pkg.nix
+++ nix/pkg.nix
@@ -1,27 +1,40 @@
-{ mkDerivation, async, attoparsec, base, bytestring, chronos
-, concur-core, concur-replica, containers, envparse, esqueleto
-, extra, file-embed, hpack, hspec, hspec-wai, katip, lens
-, lens-simple, microlens, monad-logger, mtl, persistent
-, persistent-migration, persistent-postgresql, persistent-template
-, rainbow, replica, resource-pool, retry, stdenv, stm
-, template-haskell, text, time, unbounded-delays, universum
-, unliftio, wai, wai-middleware-static-embedded, warp, websockets
+{ mkDerivation, async, attoparsec, base, bytestring, casing
+, chronos, concur-core, concur-replica, containers, directory
+, directory-tree, envparse, esqueleto, extra, file-embed, filepath
+, hpack, hspec, hspec-wai, katip, lens, lens-simple, microlens
+, monad-logger, mtl, persistent, persistent-migration
+, persistent-postgresql, persistent-template, rainbow, replica
+, resource-pool, retry, stdenv, stm, template-haskell, text
+, th-lift-instances, time, unbounded-delays, universum, unliftio
+, wai, wai-middleware-static-embedded, warp, websockets
 }:
 mkDerivation {
   pname = "haskii";
   version = "0.1.0.0";
   src = ./..;
+  isLibrary = true;
+  isExecutable = true;
   libraryHaskellDepends = [
-    async attoparsec base bytestring chronos concur-core concur-replica
-    containers envparse esqueleto extra file-embed hspec hspec-wai
-    katip lens lens-simple microlens monad-logger mtl persistent
+    async attoparsec base bytestring casing chronos concur-core
+    concur-replica containers directory directory-tree envparse
+    esqueleto extra file-embed filepath hspec hspec-wai katip lens
+    lens-simple microlens monad-logger mtl persistent
     persistent-migration persistent-postgresql persistent-template
-    rainbow replica resource-pool retry stm template-haskell text time
-    unbounded-delays universum unliftio wai
+    rainbow replica resource-pool retry stm template-haskell text
+    th-lift-instances time unbounded-delays universum unliftio wai
     wai-middleware-static-embedded warp websockets
   ];
   libraryToolDepends = [ hpack ];
-  doHaddock = false;
+  executableHaskellDepends = [
+    async attoparsec base bytestring casing chronos concur-core
+    concur-replica containers directory directory-tree envparse
+    esqueleto extra file-embed filepath katip lens lens-simple
+    microlens monad-logger mtl persistent persistent-migration
+    persistent-postgresql persistent-template rainbow replica
+    resource-pool retry stm template-haskell text th-lift-instances
+    time unbounded-delays universum unliftio wai
+    wai-middleware-static-embedded warp websockets
+  ];
   prePatch = "hpack";
   homepage = "https://github.com/tkachuk-labs/haskii#readme";
   description = "Functional, Monadic Ascii-Art";
